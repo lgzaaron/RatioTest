68e8046fef89b619cfd80131ce0139cec999d122
lgzaaron <79000849@qq.com>
2019-05-22 09:28:09 +0800

    单例

--- mylib/src/main/java/com/example/mylib/LgzTestUtils.java
+++ mylib/src/main/java/com/example/mylib/LgzTestUtils.java
@@ -7,7 +7,7 @@ public class LgzTestUtils {
     private LgzTestUtils() {
     }
     private static LgzTestUtils util;
-    private static LgzTestUtils getInstance(){
+    public static LgzTestUtils getInstance(){
         if (util==null){
             synchronized (LgzTestUtils.class){
                 if (util==null){
